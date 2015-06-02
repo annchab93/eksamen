dd32bf3cbbe0df1543b4369bc1b7a37ad1e94055
annchab93 <Anncha@Ann-Charlotts-MacBook-Air.local>
2015-06-02 14:46:25 +0200

    created models for kvitter_messages

--- kvitter/kvitter_messages/models.py
+++ kvitter/kvitter_messages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Message(models.Model):
+    user = models.ForeignKey(User, related_name='messages')
+    message = models.TextField()
+    likes = models.PositiveIntegerField(default=0)
+    created_by = models.ForeignKey(User, related_name="created_messages")
+    created_datetime = models.DateTimeField()
+
+    def __unicode__(self):
+        return u'%s' % self.note
+
+    class Meta:
+        ordering = ['-id']
