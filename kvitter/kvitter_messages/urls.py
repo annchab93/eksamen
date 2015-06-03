dcd3dec8942acbe948bbcb86fab7030d4edb94a9
annchab93 <Anncha@Ann-Charlotts-MacBook-Air.local>
2015-06-03 02:51:04 +0200

    added url likes

--- kvitter/kvitter_messages/urls.py
+++ kvitter/kvitter_messages/urls.py
@@ -5,8 +5,9 @@ from kvitter_messages import views
 urlpatterns = patterns('',
     url(r'^$', views.message_listing, name='message_listing'),
     url(r'^(\d+)/$', views.message_details, name='message_details'),
-    
-)   
+    url(r'^(\d+)/add_likes$', views.add_likes, name='add_likes'),   
+) 
+  
 
 
 
